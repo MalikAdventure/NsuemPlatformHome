cf6768790dc202fa3c64fd6a910c7f1a8824b7e3
Anton <antosha-malik@yandex.ru>
2023-02-03 14:48:21 +0700

    Create

---
+++ not work/vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(dirname, 'index.html'),
+				news: resolve(dirname, 'news.html'),
+				new: resolve(__dirname, 'new.html'),
+			},
+		},
+	},
+});
